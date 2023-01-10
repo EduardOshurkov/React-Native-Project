22da617dd5d435a402936bc8e5307fb6effdc203
EduardOshurkov <drd2993@gmail.com>
2023-01-10 19:57:52 +0200

    added icons for screens

--- Route/router.js
+++ Route/router.js
@@ -8,22 +8,46 @@ import PostScreen from '../Screens/main/PostScreen';
 import ProfileScreen from '../Screens/main/ProfileScreen';
 
 
+// icons import
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+
 const AuthStack = createStackNavigator();
 const MainTab = createBottomTabNavigator();
 
 export const useRoutes = (isAuth) => {
-  if (!isAuth) {
+    if (!isAuth) {
+        return (
+            <AuthStack.Navigator>
+                <AuthStack.Screen options={{ headerShown: false, }} name='Login' component={LoginScreen} />
+                <AuthStack.Screen options={{ headerShown: false }} name='Registration' component={RegistrationScreen} />
+            </AuthStack.Navigator>
+        )
+    }
     return (
-    <AuthStack.Navigator>
-        <AuthStack.Screen options={{ headerShown: false, }} name='Login' component={LoginScreen} />
-        <AuthStack.Screen options={{ headerShown: false }} name='Registration' component={RegistrationScreen} />
-      </AuthStack.Navigator>
-  )}
-  return (
-    <MainTab.Navigator tabBarOptions={{showLabel: false}}>
-      <MainTab.Screen options={{ headerShown: false }} name='CreateScreen' component={CreateScreen} />
-      <MainTab.Screen options={{ headerShown: false }} name='PostCreen' component={PostScreen} />
-      <MainTab.Screen options={{ headerShown: false }} name='ProfileScreen' component={ProfileScreen} />
-    </MainTab.Navigator>
-  )
+        <MainTab.Navigator tabBarOptions={{ showLabel: false }}>
+            <MainTab.Screen options={{
+                headerShown: false,
+                tabBarIcon: ({ focused, size, color }) => (
+                    <MaterialCommunityIcons name="postage-stamp" size={size} color={color} />
+                )
+            }}
+                name='PostScreen' component={PostScreen} />
+            <MainTab.Screen options={{
+                headerShown: false,
+                tabBarIcon: ({ focused, size, color }) => (
+                    <AntDesign name="pluscircle" size={size} color={color} />
+                )
+            }}
+                name='CreateScreen' component={CreateScreen} />
+            <MainTab.Screen options={{
+                headerShown: false,
+                tabBarIcon: ({ focused, size, color }) => (
+                    <Feather name="user" size={size} color={color} />
+                )
+            }}
+                name='ProfileScreen' component={ProfileScreen} />
+        </MainTab.Navigator>
+    )
 };
